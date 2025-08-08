c22b6667baaf24192f46513fcc0d3d6bfad73bcf
Christian-hub-art <christianbecerra@javeriana.edu.co>
2025-08-07 20:35:40 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,10 +1,10 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-float imcFuntion(float masa, float altura);
-void impMatriz(float mA, int N);
-float matrixMult(int N, float mA, float mB);
-void llenarMatriz (int N, float mA, float mB);
-int printFuntion();
+float imcFuntion(float masa, float altura); // Calcula el índice de masa corporal 
+void impMatriz(float mA, int N); // Imprime la matriz mA de tamaño N
+float matrixMult(int N, float mA, float mB); // Multiplica dos matrices de tamaño N
+void llenarMatriz (int N, float mA, float mB); // Llena las matrices con valores
+int printFuntion(); // Muestra un mensaje o valor
 
 #endif
